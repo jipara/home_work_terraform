a23fddc2c70556cd90c0a359fbf9c0202bccd863
jipara <zhypargul.esengulova@gmail.com>
2019-08-07 14:26:36 -0500

    aaa

--- instance.tf
+++ instance.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web1" {
-  ami             = "{vars.ami}"
-  instance_type   = "{vars.instance_type}"
+  ami             = "{var.ami}"
+  instance_type   = "{var.instance_type}"
   key_name        = "${aws_key_pair.terraform_april.key_name}"
   security_groups = ["${aws_security_group.sec-1.name}"]
   user_data       = "${file("userdata.sh")}"
