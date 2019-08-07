02808e817196fcb588e51dc088ac7b382b80587a
jipara <zhypargul.esengulova@gmail.com>
2019-08-07 14:16:19 -0500

    aa

---
+++ sec_group.tf
@@ -0,0 +1,28 @@
+resource "aws_security_group" "sec-1" {
+  name        = "sec-1"
+  description = "Allow SSH"
+  vpc_id      = "vpc-9fe64de7"
+
+  ingress {
+    # TLS (change to whatever ports you need)
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    # Please restrict your ingress to only necessary IPs and ports.
+    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name       = "Dev"
+    Dept       = "IT"
+    Group      = "April"
+    Created_by = "Jipara"
+  }
+}
