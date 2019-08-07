a0cf2f25baefd8d2cc6728cd0e88b20fa70b6af3
jipara <zhypargul.esengulova@gmail.com>
2019-08-07 13:42:48 -0500

    aaa

---
+++ instance.tf
@@ -0,0 +1,17 @@
+resource "aws_instance" "web1" {
+  ami             = "{vars.ami}"
+  instance_type   = "{vars.instance_type}"
+  key_name        = "${aws_key_pair.terraform_april.key_name}"
+  security_groups = ["${aws_security_group.sec-1.name}"]
+  user_data       = "${file("userdata.sh")}"
+
+
+
+
+  tags = {
+    Name       = "Wordpress"
+    Dept       = "IT"
+    Group      = "April"
+    Created_by = "Jipara"
+  }
+}
